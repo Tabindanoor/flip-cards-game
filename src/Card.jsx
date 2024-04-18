db6ea0d0b28a54ad1c381647a01cee51628a2b5a
TabindaNoor <tabindanoor415@gmail.com>
2024-04-18 21:14:17 +0500

    done

--- src/Card.jsx
+++ src/Card.jsx
@@ -27,8 +27,8 @@ const Card = ({ card, onClick, disabled, matched }) => {
       className={`card ${flipped || matched ? "flipped" : ""}`}
       onClick={handleClick}
     >
-      <div className="bg-violet-700 border-red-500 border-4 rounded-lg p-4 w-20 h-20 flex justify-center items-center text-4xl font-bold">
-        {flipped || matched ? card.icon : "🤔"}
+      <div className="bg-violet-700 border-green-500 border-4 rounded-lg p-4 w-20 h-20 flex justify-center items-center text-4xl font-bold">
+        {flipped || matched ? card.icon : "😎"}
       </div>
     </div>
   );
