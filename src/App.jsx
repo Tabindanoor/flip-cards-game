3baea956433f52d0df4784567468421b35929070
TabindaNoor <tabindanoor415@gmail.com>
2024-04-19 19:15:12 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css"
 
 function App() {
   return (
-    <div className=" cursor-pointer mx-auto justify-center text-center App ">
+    <div className=" cursor-pointer mx-auto justify-center text-center App text-fuchsia-900 font-serif ">
       <h1 className="text-3xl font-bold text-center ">Memory Card Game</h1>
       < Game/>
     </div>
